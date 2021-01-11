2d21d47b449713e2bebdb16a7b19fa0a82fa334e
Daniel Robertson <danieljrobertson@gmail.com>
2021-01-11 23:48:13 +0000

    add authenticate_with_credentials method to User model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,16 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
-  
+  validates :email, presence: true, :uniqueness => { :case_sensitive => false }
+  validates :password, presence: { on: create }, length: { minimum: 8 }
 
+  private
+  def self.authenticate_with_credentials email, password
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,59 @@ RSpec.describe User, type: :model do
       expect(@user.save).to eq false
       expect(@user.errors.messages[:first_name]).to eq ["can't be blank"]
     end
+    it 'fails to save when last name field is omitted' do
+      @user.last_name = nil
+      expect(@user.save).to eq false
+      expect(@user.errors.messages[:last_name]).to eq ["can't be blank"]
+    end
+    it 'fails to save when email field is omitted' do
+      @user.email = nil
+      expect(@user.save).to eq false
+      expect(@user.errors.messages[:email]).to eq ["can't be blank"]
+    end
+    it 'fails to save when the password and password confirmation do not match' do
+      @user.password_confirmation = 'swordfish'
+      expect(@user.save).to eq false
+      expect(@user.errors.messages[:password_confirmation]).to eq ["doesn't match Password"]
+    end
+    it 'fails to save when the email address is already in the database' do
+      expect(@user.save).to eq true
+      @user_two = User.new(
+        first_name: 'Bob',
+        last_name: 'Danson', 
+        email: 'bobdanson@me.co', 
+        password: 'sw0rdf1sh', 
+        password_confirmation: 'sw0rdf1sh'
+      )
+    expect(@user_two.save).to eq false
+    expect(@user_two.errors.messages[:email]).to eq ["has already been taken"]
+    end
+    it 'fails to save when the password has a length less than eight characters' do
+      @user.password = '1234'
+      @user.password_confirmation = '1234'
+      expect(@user.save).to eq false
+    end
+  end
+  describe 'Authenticate with credentials' do
+    before :each do
+      @user = User.create(
+        first_name: 'Bob',
+        last_name: 'Danson', 
+        email: 'bobdanson@me.co', 
+        password: 'sw0rdf1sh', 
+        password_confirmation: 'sw0rdf1sh')
+    end
+    it 'returns a user when correct credentials are submitted' do
+      @user_two = User.authenticate_with_credentials('bobdanson@me.co', 'sw0rdf1sh')
+      expect(@user_two.last_name).to eq ('Danson')
+    end
+     it 'returns nil when email cannot be found' do
+      @user_two = User.authenticate_with_credentials('danbobson@me.co', 'sw0rdf1sh')
+      expect(@user_two).to be_nil
+    end
+    it 'returns nil when password does not match' do
+      @user_two = User.authenticate_with_credentials('bobdanson@me.co', 'swordfish')
+      expect(@user_two).to be_nil
+    end
   end
 end
